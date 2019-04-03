225a324d68ad57617326539bff9b024f008e2551
Da-Juan <rouanet.n@gmail.com>
2019-04-03 11:50:51 +0200

    jack_mute: Update script after upgrading to 18.10

--- bin/jack_mute.sh
+++ bin/jack_mute.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # shellcheck disable=SC2009
-PULSEUSER="$(ps -ef | /bin/grep "[p]ulseaudio --start" | /usr/bin/awk '{print $1}')"
+PULSEUSER="$(ps -ef | /bin/grep "[p]ulseaudio" | tail -1 | /usr/bin/awk '{print $1}')"
 PULSEUSERID="$(/usr/bin/id -u "$PULSEUSER")"
 PULSE_RUNTIME_PATH=/run/user/"$PULSEUSERID"/pulse
 STATUS="$(echo "$@" | /bin/sed -E 's/.*\s((un)?plug).*/\1/')"
@@ -15,7 +15,7 @@ case "$STATUS" in
 		exit 0
 		;;
 esac
-SINKS=( $(/bin/su "$PULSEUSER" /bin/bash -c "PULSE_RUNTIME_PATH=$PULSE_RUNTIME_PATH /usr/bin/pacmd list-sinks" | /bin/sed -n -E 's/^\s+\*?\s+index:\s([0-9]+)/\1/p') )
+mapfile -t SINKS <<< "$(/bin/su "$PULSEUSER" /bin/bash -c "PULSE_RUNTIME_PATH=$PULSE_RUNTIME_PATH /usr/bin/pacmd list-sinks" | /bin/sed -n -E 's/^\s+\*?\s+index:\s([0-9]+)/\1/p')"
 for i in "${SINKS[@]}"; do
 	/bin/su "$PULSEUSER" /bin/bash -c "PULSE_RUNTIME_PATH=$PULSE_RUNTIME_PATH /usr/bin/pacmd set-sink-mute $i $MUTE"
 done
